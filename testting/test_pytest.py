299acc7f61258a83a44897e74ace0973fed6d7b4
yd <>
2020-03-24 22:13:34 +0800

    新增 add 和 div的 testcase
    Signed-off-by: yd <>

--- testting/test_pytest.py
+++ testting/test_pytest.py
@@ -5,8 +5,20 @@ class TestCalc:
     def setup(self):
         self.calc = Calc()
 
-    def test_add(self):
+    def test_add_1(self):
         assert self.calc.add(1, 2) == 3
 
-    def test_div(self):
-        assert self.calc.div(1, 2) == 0.8
+    def test_add_2(self):
+        assert self.calc.add(1, 3) == 5
+
+    def test_add_3(self):
+        assert self.calc.add(0, 0.5) == 2
+
+    def test_div_1(self):
+        assert self.calc.div(1, 2) == 0.5
+
+    def test_div_2(self):
+        assert self.calc.div(1, 0) == 0
+
+    def test_div_3(self):
+        assert self.calc.div(0.5, 0.5) == 1
